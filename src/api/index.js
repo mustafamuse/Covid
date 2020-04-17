1f13145187a79dacbde2bef59758ff0cbf85a324
Mustafa Muse <umpp101@icloud.com>
2020-04-16 20:41:11 -0700

    mapped thru my fetched DailyData, created a line chart, passdown the props

--- src/api/index.js
+++ src/api/index.js
@@ -17,8 +17,15 @@ export const fetchDailyData = async () =>{
     try {
 
         const {data} = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }));
+        
+        return modifiedData
 
-        console.log(data)
+        console.log('fetch Daily Data: ', modifiedData)
         
     } catch (error) {
         
--- src/components/charts/Charts.js
+++ src/components/charts/Charts.js
@@ -1,19 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import { fetchDailyData } from "../../api"
+import { Line, Bar } from "react-chartjs-2"
+import styles from "./Charts.module.css"
 const Charts = () => {
-    const [dailyData, setDailyData] = useState({})
+    const [dailyData, setDailyData] = useState([])
 
     useEffect(() => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData())
         }
             fetchAPI()
-        
     })
 
+    const lineChart  = (
+        dailyData[0] ?
+        (
+        <Line
+        data={{
+            // deconstruct the date, map and return an array
+            labels: dailyData.map(({ date }) => date),
+            // api doesn't have daily recovered data, it just has the daily infected and daily deaths only
+            datasets: [{ data: dailyData.map(({ confirmed }) => confirmed), labeled: "Infected", borderColor: "#3333ff", fill: true }, 
+                       { data: dailyData.map(({ deaths }) => deaths), labeled: "Deaths", borderColor: "red", fill: true, backgroundColor: "red"}],
+        }}
+        />)
+        : null
+    )
+
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Chart</h1>
+            {lineChart}
         </div>
     )
 } 
